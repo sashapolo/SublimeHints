c7fbba5f35ad700f1df2811bb9e677b3e1a9f629
Mikhail Golubev <qsolo825@gmail.com>
2013-04-01 20:13:13 +0400

    Added SublimeUtilMixin helper class with some common methods.

--- SublimeHints.py
+++ SublimeHints.py
@@ -4,7 +4,6 @@ import os
 import sys
 from hints import *
 import logging
-from subprocess import call
 
 import sublime
 import sublime_plugin
@@ -13,6 +12,8 @@ import sublime_plugin
 PLUGIN_DIRECTORY = os.path.join(sublime.packages_path(), __name__)
 if PLUGIN_DIRECTORY not in sys.path:
     sys.path.insert(0, PLUGIN_DIRECTORY)
+
+import nose
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -114,13 +115,13 @@ class DumbRendererCommand(HintsRenderer):
         self.window.show_quick_panel([hint.text for hint in hints_file.hints],
                                      on_load)
 
-
 class ShowPathCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         import pprint
-
         pprint.pprint(sys.path)
 
 class TestPluginCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        call(["nosetests", "--with-nosexunit"])
+        import testhintsload
+        testhintsload._current_view = self.view
+        nose.run(argv=['--where', PLUGIN_DIRECTORY])
--- testhintsload.py
+++ testhintsload.py
@@ -1,23 +1,21 @@
-import json
 import logging
-#from nose.tools import *
 import os
-import sys
 
-from hints import *
+from hints import HintFile, HintFormatError
+from nose.tools import raises
 
-class TestHintsLoader():
-    """ Hints loader tests
-    """
+_current_view = None
 
-#    @raises(HintFormatError)
+class TestHintsLoader:
+    """Hints loader tests"""
+
+    @raises(HintFormatError)
     def test_missing_hints(self):
-        """ Asserts that HintFormatError is raised when given JSON
+        """Asserts that HintFormatError is raised when given JSON
         doesn't have "hints" key
         """
         hints_file_name = "no-hints.hints"
         if not os.path.exists(hints_file_name):
             logging.info("Hint file %s not found", hints_file_name)
             return
-        with open(hints_file_name, 'r') as hints_file:
-            HintFile.load_json(self.view, hints_file)
+        HintFile.load_json(_current_view, hints_file_name)
