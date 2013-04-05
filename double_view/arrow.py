828e949a02a138ef971a4469fb3c5f9393a0a74e
SamWot <samkolton@gmail.com>
2013-04-05 19:39:31 +0400

    #14 Completed arrow routing.

--- double_view/arrow.py
+++ double_view/arrow.py
@@ -8,55 +8,94 @@ class Arrow(object):
         self.start = start_line
         self.end = end_line
 
+class RoutedArrow(object):
+    def __init__(self, arrow):
+        """
+        start - (row, col) coordinates of arrow start point
+        end - (row. col) coordinates of arrow end point
+        vertical - number of column with vertical line of arrow
+        """
+        self.start = (arrow.start, 0)
+        self.end = (arrow.end, 0)
+        self.vertical = 0
 
 class SimpleRouter(object):
     def __init__(self, arrows):
-        self.arrows = arrows
+        self.arrows = map(RoutedArrow, arrows)
+        
+    class RoutingInfo(object):
+        def __init__(self, arrow):
+            self.arrow = arrow
+
 
     def route(self):
         self.arrows.sort(__sort_function__)
-        width1 = self.__single_line_width__()
-        width2 = self.__parallel_arrows_width__()
+        width1 = self.__same_line__()
+        width2 = self.__parallel_arrows__()
         self.width = max([width1, width2])
 
     def __sort_function__(x, y):
-        if x.start < y.start:
+        if x.start[0] < y.start[0]:
             return -1
-        elif x.start == y.start:
-            if x.end < y.end:
+        elif x.start[0] == y.start[0]:
+            if x.end[0] < y.end[0]:
                 return -1
-            elif x.end == y.end:
+            elif x.end[0] == y.end[0]:
                 return 0
             else:
                 return 1
         else:
             return 1
 
-    def __single_line_width__(self):
-        maxcount = 0
-        curcount = 0
-        curpos = 0
+    def __same_line__(self):
+        curpos = -1
+        groups_size = dict()
+        routings = []
         for arrow in self.arrows:
-            if arrow.start == curpos:
-                curcount += 1
+            if arrow.start[0] == curpos:
+                groups_size[curpos] += 1                   
             else:
-                curpos = arrow.start
-                curcount = 1
-            if curcount > maxcount:
-                    maxcount = curcount
+                curpos = arrow.start[0]
+                groups_size[curpos] = 1
+            routing = RoutingInfo(arrow)
+            routing.same_line_number = groups_size[curpos] 
+            routings.append(routing)
+        maxcount = max(groups_size, key = lambda x: groups_size[x])
+        
+        for routing in routings:
+            size = groups_size[routing.arrow.start[0]]
+            number = routing.same_line_number
+            routing.arrow.start[1] = 2 * (size - number)
+
+        self.arrow = map(lambda x: x.arrow, routings)
         return 3 + (maxcount - 1) * 2
 
-    def __parallel_arrows_width__(self):
-        maxcount = 0
-        curcount = 0
-        curend = 0
+    def __parallel_arrows__(self):
+        curend = -1
+        groups_size = dict()
+        i = 0
+        routings = []
         for arrow in self.arrows:
-            if arrow.start <= curend:
-                curcount += 1
-                curend = arrow.end
+            if arrow.start[0] <= curend:
+                groups_size[i] += 1
+                curend = arrow.end[0]
             else:
-                curend = arrow.end
-                curcount = 1
-            if curcount > maxcount:
-                    maxcount = curcount
+                curend = arrow.end[0]
+                groups_size[i] = 1
+            routing = RoutingInfo(arrow)
+            routing.group_number = i
+            routing.parallel_number = groups_size[i]
+            routings.append(routing)
+            i += 1
+        maxcount = max(groups_size, key = lambda x: groups_size[x])
+
+        for routing in routings:
+            size = groups_size[routing.group_number]
+            number = routing.parallel_number
+            vertical = 1 + size - number
+            if vertical <= routing.arrow.start[1]:
+                vertical = routing.arrow.start[1] + 1
+            routing.arrow.vertical = vertical
+
+        self.arrow = map(lambda x: x.arrow, routings)
         return 3 + (maxcount - 1)
