0aef3f5b6c82e289612c6877c80272ce868dd17a
yaromir <yaromir@hotbox.ru>
2013-04-19 12:38:13 +0400

    #20 Fixed some bugs in tests

--- examples/test_hints_all_correct_pydict.py
+++
@@ -1,9 +0,0 @@
-"""
-Python dictionary equivalent of the hints_all_correct hint file.
-Used for testing.
-"""
-
-hints_all_correct = dict(
-    'created' = '2013-03-31:17:50:00',
-    'author' = 'Skotnikov'
-)
---
+++ test/hintfiles/correct_pydict.py
@@ -0,0 +1,9 @@
+"""
+Python dictionary equivalent of the hints_all_correct hint file.
+Used for testing.
+"""
+
+filedata = {
+    'created': '2013-03-31:17:50:00',
+    'author': 'Skotnikov'
+}
